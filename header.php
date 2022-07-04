9e827059f1a3c03cf28b745d63c3de3454dafa12
mridha-emran <bari.lala490@gmail.com>
2022-07-04 10:03:43 +0200

    first commit

---
+++ header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <title>News</title>
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <!-- Font Awesome Icon -->
+    <link rel="stylesheet" href="css/font-awesome.css">
+    <!-- Custom stlylesheet -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<div id="menu-bar">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <ul class='menu'>
+                    <li><a href='#'>Business</a></li>
+                    <li><a href='#'>Entertainment</a></li>
+                    <li><a href='#'>Sports</a></li>
+                    <li><a href='#'>Politics</a></li>
+                   
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+
