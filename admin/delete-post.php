8cd5bb2fb7a501d8c8ca1a52a1c170f7a1056c74
mridha-emran <bari.lala490@gmail.com>
2022-07-20 09:34:08 +0200

    delete image from upload folder

--- admin/delete-post.php
+++ admin/delete-post.php
@@ -2,14 +2,23 @@
                   // mysql connaction
                     include "config.php";
 
-                    // getting id from URL
-                       $postId=$_GET["id"];
-                       $catId=$_GET["catid"];
-                    // query for delete post data from the database
-                     $getUSer ="DELETE FROM post WHERE post_id = {$postId};";
-                     $getUSer .="UPDATE category SET post=post-1 WHERE category_id = {$catId};";
+                    // getting ids from URL
+                    $postId=$_GET["id"];
+                    $catId=$_GET["catid"];
 
-                     $result= mysqli_multi_query($conn,$getUSer);
+                    // get user post data from the database
+                    $getPost ="SELECT * FROM post WHERE post_id = {$postId}";
+                    $postResult = mysqli_query($conn,$getPost);
+                    $row = mysqli_fetch_assoc($postResult);
+
+                    // to delete image from upload folder
+                    unlink("upload/".$row['post_img']);
+                    
+                    // query for delete post data from the database
+                     $deletPost ="DELETE FROM post WHERE post_id = {$postId};";
+                     $deletPost .="UPDATE category SET post=post-1 WHERE category_id = {$catId};";
+
+                     $result= mysqli_multi_query($conn,$deletPost);
                         //   to direct the page
                         if($result){
                              header("location: http://localhost/news-site/admin/post.php");
